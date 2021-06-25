1ab5c48ddc6c389c9d3d422bbec5bcf3b38ee49d
sagar-aryal <sagar_yal92@outlook.com>
2021-06-25 18:15:54 +0300

    working with arrays and functions

--- JS from zero to expert/Problem 1/scripts.js
+++ JS from zero to expert/Problem 1/scripts.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// Exercise 1
+
 /*
 
 We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day.
@@ -20,23 +22,68 @@ const temperature = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
 */
 
-const temp = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+// const temp = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
 
 // Creating function to find the maximum and minimum value in temperature array.
-const tempAmp = (temps) => {
-  let maxTemp = 0;
-  let minTemp = 0;
-  for (let i = 0; i < temps.length; i++) {
-    if (temps[i] > maxTemp) {
-      maxTemp = temps[i];
-    }
-    if (temps[i] < minTemp) {
-      minTemp = temps[i];
-    }
+// const tempAmp = (temps) => {
+//   let maxTemp = 0;
+//   let minTemp = 0;
+//   for (let i = 0; i < temps.length; i++) {
+//     if (temps[i] > maxTemp) {
+//       maxTemp = temps[i];
+//     }
+//     if (temps[i] < minTemp) {
+//       minTemp = temps[i];
+//     }
+//   }
+//   console.log(maxTemp, minTemp);
+//   return maxTemp - minTemp;
+// };
+
+// const amplitude = tempAmp(temp);
+// console.log(amplitude);
+
+// Exercise 2
+// Converting temperature from degree to kelvin.
+
+// const measureKelvin = () => {
+//   const measurement = {
+//     type: "trmp",
+//     unit: "celsius",
+//     //value: prompt("Degrees celsius"), // NOTE: prompt method always returns string. We need to convert string to number to fix this.
+//     value: Number(prompt("Degrees celsius")),
+//   };
+//   console.log(measurement);
+//   // console.log(measurement.value);
+//   const kelvin = measurement.value + 273;
+//   return kelvin;
+// };
+// console.log(measureKelvin());
+
+// Code Challange: Problem 1
+
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+Examples: [17, 21, 23] will print "...17°C in 1 days ... 21°C in 2 days ...23°C in 3 days ..."
+
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+Test Data 1: [17, 21, 23]
+Test Data 2: [12, 5, -5, 0, 4]
+*/
+
+const printForecast = (arr) => {
+  let i = 0;
+  let str = " ";
+  while (i < arr.length) {
+    str = str + `...${arr[i]}°C in ${i + 1} days`;
+    i++;
   }
-  console.log(maxTemp, minTemp);
-  return maxTemp - minTemp;
+  return str;
 };
 
-const amplitude = tempAmp(temp);
-console.log(amplitude);
+const arr1 = [17, 21, 23];
+const arr2 = [12, 5, -5, 0, 4];
+
+console.log(printForecast(arr1));
+console.log(printForecast(arr2));
