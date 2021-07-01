697f136ebd3c21a4ea061d6062d4b874405fcebb
sagar-aryal <sagar_yal92@outlook.com>
2021-07-01 16:46:01 +0300

    dice images 1-6 added.

--- JS from zero to expert/Projects/3. Pig Game/script.js
+++ JS from zero to expert/Projects/3. Pig Game/script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const rollDice = document.querySelector(".roll-dice");
+let currentRoll = 0;
+
+// creating a function for rolling the dice
+rollDice.addEventListener("click", () => {
+  const dice = Math.trunc(Math.random() * 6 + 1);
+  console.log(dice);
+  document.querySelector(`current`);
+});
