2c1a05d9ddb90b14c3c21f948bd0c8e907df358d
sagar-aryal <sagar_yal92@outlook.com>
2021-07-22 19:20:46 +0300

    destructuring arrays examples

--- JS from zero to expert/Data Structures/script.js
+++ JS from zero to expert/Data Structures/script.js
@@ -1,1 +1,62 @@
 "use strict";
+// Destructuring arrays
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+};
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+// console.log(a, b, c);
+
+const [x, y, z] = arr; // Destructing of an array
+console.log(a, b, c);
+
+// const [first, second] = restaurant.categories;
+// console.log(first, second);
+
+let [first, , third] = restaurant.categories; // Skiping elements inside an array we simply leave a hole in destructing structure.
+console.log(first, third);
+
+// Switching elements within an array
+[first, , third] = [third, , first];
+console.log(first, third);
+
+// Receive two return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+// Nested destructuring
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// Default values
+// const [p, q, r] = [8, 9];
+// console.log(p, q, r); // r will be undefined
+const [p = 1, q = 1, r = 1] = [8, 9]; // assigning default values if its not assigned.
+console.log(p, q, r); // r will be 1 cos its default value is 1
+
+restaurant.orderDelivery({
+  starterIndex: 2,
+  mainIndex: 0,
+  address: "Rio de Helsinki",
+  time: 11.25,
+});
