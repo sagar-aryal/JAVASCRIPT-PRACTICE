596ad6bed41cd6d20cf0cd68a6e8d1742e678121
sagar-aryal <sagar_yal92@outlook.com>
2021-08-02 15:08:41 +0300

    adding padding effect to make string readable

--- JS from zero to expert/Data Structures/script.js
+++ JS from zero to expert/Data Structures/script.js
@@ -763,11 +763,11 @@ console.log(flights.split("+"));
 
 for (const flight of flights.split("+")) {
   const [type, from, to, time] = flight.split(";");
-  const output = `${type.startsWith("_Delayed") ? "🔴" : "  "}${type.replaceAll(
+  const output = `${type.startsWith("_Delayed") ? "🔴" : ""}${type.replaceAll(
     "_",
     " "
   )} from ${from.slice(0, 3).toUpperCase()} to ${to
     .slice(0, 3)
-    .toUpperCase()} (${time.replace(":", "h")})`;
+    .toUpperCase()} (${time.replace(":", "h")})`.padStart(45);
   console.log(output);
 }
