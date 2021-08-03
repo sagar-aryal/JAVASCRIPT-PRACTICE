a51efb6ec14251c1ba5dfede6f39da247a04f874
sagar-aryal <sagar_yal92@outlook.com>
2021-08-03 15:45:08 +0300

    first-class, high-order and callback function

---
+++ JS from zero to expert/Advance Functions/script.js
@@ -0,0 +1,73 @@
+"use strict";
+
+/*
+const bookings = [];
+
+const createBooking = function (
+  flightName,
+  numPassengers = 1, // we can write default value directly here inide parenthesis.
+  price = 199 * numPassengers
+) {
+  const booking = {
+    flightName,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking("LH123", 5);
+createBooking("LH123", 5, 800);
+createBooking("LH123", undefined, 1000); // if we need to use default value, we need to use undefined.
+
+console.log(bookings);
+*/
+
+/*
+const flight = "LM123";
+const jonas = {
+  name: "Jonas Schmedtmann",
+  passport: 23456789,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = "LH999";
+  passenger.name = "Mr.".concat(passenger.name);
+
+  passenger.passport === 23456789
+    ? alert("Checked In!")
+    : alert("Wroung passport!");
+};
+
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+*/
+
+const oneWord = function (str) {
+  return str.replace(/ /g, "").toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), ...others].join(" ");
+};
+
+// High-order function
+const transformer = function (str, fun) {
+  console.log(`Original String: ${str}`);
+  console.log(`Transformed String: ${fun(str)}`);
+  console.log(`Transformed by: ${fun.name}`);
+};
+
+transformer("JavaScript is the best", upperFirstWord);
+transformer("JavaScript is a Popular Language", oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log("👋");
+};
+document.body.addEventListener("click", high5);
+
+["Jonas", "Martha", "Adam"].forEach(high5);
