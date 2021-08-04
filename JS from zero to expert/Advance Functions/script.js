a5dcc8ef49d465d34fdb4384102892e82987d8fe
sagar-aryal <sagar_yal92@outlook.com>
2021-08-04 14:08:29 +0300

    Closures examples

--- JS from zero to expert/Advance Functions/script.js
+++ JS from zero to expert/Advance Functions/script.js
@@ -191,7 +191,7 @@ console.log(addVAT(100));
 ///////////////////////////////////////
 // Coding Challenge #1
 
-/* 
+/*
 Let's build a simple poll app!
 A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
 Here are your tasks:
@@ -203,10 +203,10 @@ Here are your tasks:
         2: Rust
         3: C++
         (Write option number)
-  
+
   1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
 2. Call this method whenever the user clicks the "Answer poll" button.
-3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1".
 4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
 HINT: Use many of the tools you learned about in this and the last section 😉
 BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
@@ -215,7 +215,7 @@ BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
 GOOD LUCK 😀
 */
 
-// 1
+/*
 const poll = {
   question: "What is your favorite programming language?",
   options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
@@ -252,3 +252,74 @@ document
 poll.displayResults.call({ answers: [1, 2, 3] });
 poll.displayResults.call({ answers: [1, 2, 3] }, "string");
 poll.displayResults.call({ answers: [1, 2, 3, 4, 5, 6] }, "string");
+*/
+
+/*
+const runOnce = function () {
+  console.log("This will never run again.");
+};
+runOnce();
+
+// Immediately invoked function expression
+(function () {
+  console.log("This will never run again and again in easy way.");
+})();
+*/
+
+// Closures
+/*
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers.`);
+  };
+};
+
+const booker = secureBooking(); // Here booker is function not a variable.
+
+booker();
+booker();
+booker();
+*/
+
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+
+// Re-assigning f function.
+h();
+f();
+
+console.dir(f);
+
+// Example 2
+const boardPassenger = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers.`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers.`);
+  }, 1000);
+
+  console.log(`Will start boarding in ${wait} seconds.`);
+};
+
+boardPassenger(100, 3);
